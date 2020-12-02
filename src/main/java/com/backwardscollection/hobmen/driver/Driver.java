fb968a5a86e4ba3ca1155f80264b2177a706d935
bwcsemaj <bwc_semaj@live.com>
2020-12-02 06:27:00 -0500

    initializing project

---
+++ src/main/java/com/backwardscollection/hobmen/driver/Driver.java
@@ -0,0 +1,8 @@
+package com.backwardscollection.hobmen.driver;
+
+public class Driver {
+    
+    public static void main(String[] args) {
+        HobmemDriver.main(args);
+    }
+}
---
+++ src/main/java/com/backwardscollection/hobmen/driver/HobmemDriver.java
@@ -0,0 +1,16 @@
+package com.backwardscollection.hobmen.driver;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HobmemDriver extends Application {
+    
+    public static void main(String[] args) {
+        HobmemDriver.launch();
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+    
+    }
+}
